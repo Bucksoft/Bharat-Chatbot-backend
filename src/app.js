f41d14d6e8aca348b7fd184753468d23076fb5b5
bucksoftech <bucksoftechpvtltd@gmail.com>
2025-07-31 15:22:59 +0530

    Audited packages

--- src/app.js
+++ src/app.js
@@ -24,7 +24,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://bharatchatbot.onrender.com",
+    ],
     credentials: true,
   })
 );
